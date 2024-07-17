42de2250b349231cdec8266023f822c6fb47328e
KEAN RAMOS <144510568+KeanCodex@users.noreply.github.com>
2024-07-17 22:40:49 +0800

    Negga si arron (#2)
    * Negga
    
    * Negg's

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Screens/Homepage.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -8,13 +10,8 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('hey hey Joel Gutlay!!'),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      );
 }
