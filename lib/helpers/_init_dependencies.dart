7cd3ec7f3f8a5a986766f642a335df54da5c0223
Livsn06 <joelgutlay04@gmail.com>
2024-07-27 17:52:55 +0800

    update

--- lib/helpers/_init_dependencies.dart
+++ lib/helpers/_init_dependencies.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:naturemedix/controllers/onboarding_controller.dart';
+import 'package:naturemedix/controllers/splash_controller.dart';
 
 class InitDep implements Bindings {
   @override
   void dependencies() {
     // TODO: implement dependencies'
     Get.lazyPut(() => OnboardingController());
+    Get.lazyPut(() => SplashController());
   }
 }
